442d812a4506bd0cda07d0b9caf4462bf5f7571e
raSel <mdraselm413@gmail.com>
2022-11-02 23:23:05 +0600

    simple server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.send('genius car surver is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Genius car running on port: ${port}`);
+})
