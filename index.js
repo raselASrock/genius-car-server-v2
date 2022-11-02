50b822d82f7c99e13dccec95470420b382bfd3e7
raSel <mdraselm413@gmail.com>
2022-11-03 00:39:50 +0600

    connect

--- index.js
+++ index.js
@@ -19,11 +19,17 @@ console.log(process.env.DB_PASSWORD);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.yyonl1x.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const surviceCollection = client.db('genius-car').collection('services')
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err))
 
 
 
