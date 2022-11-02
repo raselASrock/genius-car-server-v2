9c4947daf2c59416e9824c5372d29baebe2b3b95
raSel <mdraselm413@gmail.com>
2022-11-03 00:10:04 +0600

    basic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +9,24 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.yyonl1x.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 app.get('/', (req, res) =>{
     res.send('genius car surver is running')
